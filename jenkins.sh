1300468dedcd7076c81e5e037a4f9f2a45c86d59
mygithublab <mygithublab@126.com>
2018-04-22 14:56:29 +0800

    20180422145629

--- jenkins.sh
+++ jenkins.sh
@@ -2,14 +2,14 @@
 
 docker build --rm -t printer .
 docker rm -f printer
-docker run -itd --name printer -p 8003:22 -p 8002:80 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /tmp/$(mktemp -d):/run -v /volume2:/share --restart=always printer
+#docker run -itd --name printer -p 8003:22 -p 8002:80 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /tmp/$(mktemp -d):/run -v /volume2:/share --restart=always printer
 
-#docker run -itd --name printer -p 8003:22 -p 8002:80 \
-# --privileged \
-# -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-# -v /tmp/$(mktemp -d):/run \
-# -v $WORKSPACE/etc:/usr/local/nagios/etc \
-# -v /volume4/nagios/var:/usr/local/nagios/var \
-# -v /volume4/nagiosgraph/var:/usr/local/nagiosgraph/var \
-# -v /volume2:/share \
-# --restart=always printer
+docker run -itd --name printer -p 8003:22 -p 8002:80 \
+ --privileged \
+ -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+ -v /tmp/$(mktemp -d):/run \
+ -v $WORKSPACE/etc:/usr/local/nagios/etc \
+ -v /volume4/nagios/var:/usr/local/nagios/var \
+ -v /volume4/nagiosgraph/var:/usr/local/nagiosgraph/var \
+ -v /volume2:/share \
+ --restart=always printer
